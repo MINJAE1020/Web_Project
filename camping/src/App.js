9427fbdf2f4329d121f7e3e51de0b6d1e34bf443
MINJAE <mjpark1124@gmail.com>
2024-06-11 22:49:17 +0900

    adf

--- camping/src/App.js
+++ camping/src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // BrowserRouter와 Routes로 변경
+import CampHomePage from "./components/customer/CampHomePage";
+import CampHomePage from "./components/customer/CustPage";
+import CampRegisterPage from "./components/host/CampRegisterPage";
+import SiteRegisterPage from "./components/host/SiteRegisterPage";
+import HostPage from "./components/host/HostPage";
+import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
+import ReviewPage from "./components/ReviewPage";
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/camp_home" element={<LoginForm />} />
-                <Route path="/camp_detail" element={<LoginForm />} />
-                <Route path="/camp_book" element={<LoginForm />} />
-                <Route path="/cust" element={<LoginForm />} />
-                <Route path="/camp_register" element={<LoginForm />} />
-                <Route path="/site_register" element={<LoginForm />} />
-                <Route path="/host" element={<LoginForm />} />
-                <Route path="/login" element={<LoginForm />} />
-                <Route path="/signup" element={<LoginForm />} />
-                <Route path="/review" element={<LoginForm />} />
+                <Route path="/camp_home" element={<CampHomePage />} />
+                <Route path="/camp_detail" element={<CampDetailPage />} />
+                <Route path="/camp_book" element={<CampDetailPage />} />
+                <Route path="/cust" element={<CustPage />} />
+                <Route path="/camp_register" element={<CampRegisterPage />} />
+                <Route path="/site_register" element={<SiteRegisterPage />} />
+                <Route path="/host" element={<HostPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route path="/review" element={<ReviewPage />} />
             </Routes>
         </Router>
     );
